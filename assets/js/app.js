1e43fad7b20196ee026bbb2d3157b2df02032d75
Abhishek Shenoy <abhishekshenoy@hotmail.co.uk>
2024-04-15 08:03:09 +0100

    Add longshort and add code formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,206 +1,202 @@
 
 function arrayColumn(arr, n) {
-  return arr.map(x=> x[n]);
+	return arr.map(x => x[n]);
 }
-		
+
 function argmin(a) {
-	return a.reduce(function(lowest, next, index) {return next < a[lowest] ? index : lowest; }, 0);
+	return a.reduce(function (lowest, next, index) { return next < a[lowest] ? index : lowest; }, 0);
 }
 function argmax(a) {
-	return a.reduce(function(highest, next, index) {return next > a[highest] ? index : highest; }, 0);
+	return a.reduce(function (highest, next, index) { return next > a[highest] ? index : highest; }, 0);
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////	
 
-var simulator = new Worker("./assets/js/simulate.min.js");
-simulator.onmessage = function(event){
-	
+var simulator = new Worker("./assets/js/simulate.js");
+simulator.onmessage = function (event) {
+
 	var sim_data = event.data;
 	update_simplots(assets, sim_data, total_amount);
-	
+
 	$("#simulate").removeAttr('disabled');
 	$("#sim-loading").hide();
 	$("#sim-available").show();
-	
+
 };
 //////////////////////////////////////////////////////////////////////
 
-function update_simplots(stocks, sim_data, total_amount){
-	
+function update_simplots(stocks, sim_data, total_amount) {
+
 	[returns, risks, sharpes, portfolios, stock_counts] = sim_data;
-	
+
 	var N = stocks.length;
 	var allocate = (total_amount != 0);
 	//////////////////////////////////////////////////////////////////////
 	//////////////////////////Pareto Plot//////////////////////////
 	//////////////////////////////////////////////////////////////////////
-	function weight_label(weight_arr){
+	function weight_label(weight_arr) {
 		return JSON.stringify(math.round(math.multiply(weight_arr, 100), 1));
 	}
-	
-	function count_label(count_arr){
+
+	function count_label(count_arr) {
 		return JSON.stringify(count_arr);
 	}
-	
+
 	//TBD add sharpe ratios to this
 	var pareto_labels = [];
-	if (allocate == false){
+	if (!allocate) {
 		pareto_labels = portfolios.map(weight_label);
-	}else{
+	} else {
 		pareto_labels = stock_counts.map(count_label);
 	}
-	
+
 	var mc_points = {
-	  name: "Portfolios",
-	  x: risks,
-	  y: returns,
-	  mode: 'markers',
-	  type: 'scatter',
-	  text: pareto_labels, 
-	  marker: {
-		color: 'rgba(190, 190, 255, 1)',
-		size: math.round(math.dotMultiply(sharpes, 5))
-	  }
+		name: "Portfolios",
+		x: risks,
+		y: returns,
+		mode: 'markers',
+		type: 'scatter',
+		text: pareto_labels,
+		marker: {
+			color: 'rgba(190, 190, 255, 1)',
+			size: math.round(math.dotMultiply(sharpes, 5))
+		}
 	};
-	
+
 	var min_risk_index = argmin(risks);
 	var max_sharpe_index = argmax(sharpes);
-	
-	var min_risk = {
-	  name: "Min Risk",
-	  x: [risks[min_risk_index]],
-	  y: [returns[min_risk_index]],
-	  mode: 'markers',
-	  type: 'scatter',
-	  text: weight_label(portfolios[min_risk_index])+"</br>"+"Sharpe: "+math.round(sharpes[min_risk_index], 3),
-	  marker: {
-		color: 'rgba(0,0,0,0)',
-		size: 20,
-		line: {
-		  color: 'rgb(130, 40, 255)',
-		  width: 2
-		}
-	  },
-	}; 
-	
-	
-	var max_sharpe = {
-	  name: "Max Sharpe",
-	  x: [risks[max_sharpe_index]],
-	  y: [returns[max_sharpe_index]],
-	  mode: 'markers',
-	  type: 'scatter',
-	  text: weight_label(portfolios[max_sharpe_index])+"</br>"+"Sharpe: "+math.round(sharpes[max_sharpe_index], 3),
-	  marker: {
-		color: 'rgba(0,0,0,0)',
-		size: 20,
-		line: {
-		  color: 'rgb(13, 40, 255)',
-		  width: 2
-		}
-	  },
-	}; 
 
-	
+	var min_risk = {
+		name: "Min Risk",
+		x: [risks[min_risk_index]],
+		y: [returns[min_risk_index]],
+		mode: 'markers',
+		type: 'scatter',
+		text: weight_label(portfolios[min_risk_index]) + "</br>" + "Sharpe: " + math.round(sharpes[min_risk_index], 3),
+		marker: {
+			color: 'rgba(0,0,0,0)',
+			size: 20,
+			line: {
+				color: 'rgb(130, 40, 255)',
+				width: 2
+			}
+		},
+	};
+
+
+	var max_sharpe = {
+		name: "Max Sharpe",
+		x: [risks[max_sharpe_index]],
+		y: [returns[max_sharpe_index]],
+		mode: 'markers',
+		type: 'scatter',
+		text: weight_label(portfolios[max_sharpe_index]) + "</br>" + "Sharpe: " + math.round(sharpes[max_sharpe_index], 3),
+		marker: {
+			color: 'rgba(0,0,0,0)',
+			size: 20,
+			line: {
+				color: 'rgb(13, 40, 255)',
+				width: 2
+			}
+		},
+	};
+
+
 	var data = [mc_points, min_risk, max_sharpe];
-	var layout = {autosize: false, width: 720, height: 640, xaxis:{title: "Expected Risk/Volatility"}, yaxis:{title: "Expected Return"}};
+	var layout = { autosize: false, width: 720, height: 640, xaxis: { title: "Expected Risk/Volatility" }, yaxis: { title: "Expected Return" } };
 	Plotly.newPlot('pareto-plot', data, layout);
 	$('#pareto-plot').show();
 
 	///////////////////////////////////////////////////////////////
 	//////////////////Parallel Plot//////////////////////////
 	///////////////////////////////////////////////////////////////
-	var parallel_data = [];
-	if (allocate == false){
-		parallel_data = portfolios;
-	}else{
-		parallel_data = stock_counts;
-	}
-	
+	var parallel_data = allocate ? stock_counts : portfolios;
+
 	var dims = [];
-	for (var i=0; i<N; i++){
+	for (var i = 0; i < N; i++) {
 		var dim = {
 			values: arrayColumn(parallel_data, i),
 			label: stocks[i]
 		};
 		dims.push(dim);
 	}
-	
+
 	var data = [{
-	  type: 'parcoords',
-	  pad: [80,80,80,80],
-	  line: {
-		showscale: true,
-		color: sharpes,
-		colorscale: [[0, 'red'], [0.5, 'green'], [1, 'blue']],
-		cmax: 1.7,
-		cmin: 0.5,
-		
-	  },
-	  dimensions: dims
+		type: 'parcoords',
+		pad: [80, 80, 80, 80],
+		line: {
+			showscale: true,
+			color: sharpes,
+			colorscale: [[0, 'red'], [0.5, 'green'], [1, 'blue']],
+			cmax: 1.7,
+			cmin: 0.5,
+		},
+		dimensions: dims
 	}];
-	
-	
-	var layout = {autosize: false, width: 640, height: 640};
+
+
+	var layout = { autosize: false, width: 640, height: 640 };
 	Plotly.newPlot('parallel-plot', data, layout);
 	$('#parallel-plot').show();
 }
 
-function data_plot(timestamps, data, stocks){
+function data_plot(timestamps, data, stocks) {
 	//////////////////////////////////////////////////////////////////////
 	/////////////////////////Timeseries Plot//////////////////////////////
 	//////////////////////////////////////////////////////////////////////
 	var N = stocks.length;
-	
+
 	var traces = [];
-	for (var i=0; i < N; i++){
+	for (var i = 0; i < N; i++) {
 		var trace = {
 			type: "scatter",
 			mode: "lines",
-			x: timestamps.map(t => t*1000),
+			x: timestamps.map(t => t * 1000),
 			y: data[i],
 			name: stocks[i]
 		};
 		traces.push(trace);
 	}
-	
-	var layout = {autosize: false, width: 1280, height: 640, xaxis: {type: 'date'}};
+
+	var layout = { autosize: false, width: 1280, height: 640, xaxis: { type: 'date' } };
 	Plotly.newPlot('timeseries-plot', traces, layout);
 	$('#timeseries-plot').show();
 }
+
 /////////////////////////////////////////////////////////////////////////////////////	
+
 var timestamps = [];
 var main_data = [];
 var assets = [];
 var total_amount = 0;
 
-$( document ).ready(function() {
-	
+$(document).ready(function () {
+
 	$(".plot").hide();
 	$("#simulate").hide();
 	$("#sim-loading").hide();
 	$("#go-loading").hide();
-	
+
 	var demo_tickers = "amzn,googl,aapl,gld";
 	demo_tickers = demo_tickers.split(",");
-	for (var i=0; i<demo_tickers.length; i++){
+	for (var i = 0; i < demo_tickers.length; i++) {
 		$('#tickers').tagsinput('add', demo_tickers[i]);
 	};
-	
-	$("#go").click(function(){
+
+	$("#go").click(function () {
 		$("#simulate").hide();
 		$("#go-loading").show();
-		
-		//reset clear old data
+
+		// clear old data
 		timestamps = [];
 		main_data = [];
 		assets = [];
-		
+
 		//console.log($('#tickers').val());
 		//console.log($('#tickers').tagsinput('items'));
-		
-		var params = {stocks: $('#tickers').tagsinput('items')};
+
+		var params = { stocks: $('#tickers').tagsinput('items') };
 		$.ajax({
 			url: "https://portfolio-analyser.vercel.app/api/data",
 			type: "post",
@@ -208,40 +204,38 @@ $( document ).ready(function() {
 			contentType: 'application/json',
 			data: JSON.stringify(params),
 			success: function (data) {
-				
-				//console.log(data)
+				// console.log(data)
 				assets = data["assets"];
 				timestamps = data["timestamps"];
 				// Convert row arrays to column arrays
-				for (var i=0; i < data["assets"].length; i++){
-					// main_data.push(arrayColumn(data["prices"], i));
+				for (var i = 0; i < data["assets"].length; i++) {
 					main_data.push(data["prices"][i]);
 				}
-				
+
 				data_plot(timestamps, main_data, assets);
-				
+
 				$("#simulate").show();
 				$("#go-loading").hide();
 			}
-		}).done(function(){
+		}).done(function () {
 			$("#simulate").click();
 		});
-		
+
 	});
-	
-	$("#simulate").click(function(){
+
+	$("#simulate").click(function () {
 		$("#simulate").attr('disabled', 'disabled');
 		$("#sim-loading").show();
 		$("#sim-available").hide();
-		
+
 		total_amount = parseInt($("#total").val());
 		var prune = $("#prune").is(":checked");
+		var longshort = $("#longshort").is(":checked");
 		var free_rate = $("#free-rate").val() / 100; // free interest growth
-
-		//var sim_data = simulate(assets, main_data, free_rate, total_amount, prune);		
-		simulator.postMessage([assets, main_data, free_rate, total_amount, prune]);		
-	});
 	
+		simulator.postMessage([assets, main_data, free_rate, total_amount, prune, longshort]);
+	});
+
 	/*
 	$("#go").click().delay(10000).queue(function(){
 		$("#simulate").click();
--- assets/js/simulate.js
+++ assets/js/simulate.js
@@ -1,177 +1,180 @@
-		importScripts('https://cdnjs.cloudflare.com/ajax/libs/mathjs/6.6.0/math.min.js');
-		
-		function arr_shift(arr, n){
-			arr.splice(-1,n);
-			arr.unshift(...Array(n).fill(0));
-			return arr;
-		}
-		
-		// Column Pairwise Covariance
-		function cov(a, b){
-			var av_a = math.mean(a);
-			var av_b = math.mean(b);
-			
-			var cov = 0;
-			var n = a.length;
-			for (var i=0; i < n; i++){ 
-				cov += (a[i] - av_a) * (b[i] - av_b) ;
-			}
-			return (cov / (n-1)); 
-		}
-			
-		function calc_ret(weights, mean_returns){
-			var dotprod = math.dotMultiply(mean_returns, weights);
-			return math.sum( dotprod );
-		}
-		
-		function calc_std(weights, cov_matrix){
-			var covdotw = math.multiply(cov_matrix, weights);
-			return math.sqrt(math.multiply(math.transpose(weights), covdotw));
-		}
-		
-		function calc_ret_std(weights, mean_returns, cov_matrix){
-			//Calculate return and std dev (risk)
-			ret = calc_ret(weights, mean_returns);
-			std = calc_std(weights, cov_matrix);
-			return [ret, std];
-		}
-		
-		function calc_neg_sharpe(weights, mean_returns, cov_matrix, free_rate = 0){
-			var [p_ret, p_std] = calc_ret_std(weights, mean_returns, cov_matrix);
-			return -(p_ret - free_rate) / p_std;
-		}
-		
-		function stocks_allocation(total_amount, asset_prices, weights){
-			var count = math.floor(math.dotDivide(math.multiply(total_amount, weights), asset_prices));
-			var new_weights = math.dotDivide(count, math.sum(count));
-			return [new_weights, count];
-		}
-		
-		///////////////////////////////////////////////////////////////////////////////////////////
-		///////////////////////////////////////////////////////////////////////////////////////////
-		
-		function preprocess(stocks, data){
-			//sim requirement
-			var N = stocks.length; 
-			//sim requirement
-			var asset_prices = JSON.parse(JSON.stringify(data)).map(function(col) {return col[col.length-1]});
-			
-			var data_shift = JSON.parse(JSON.stringify(data));
-			data_shift.map(function(col) { return arr_shift(col, 1) }); // equivalent of pandas dataframe shift
-			var data_returns = math.dotDivide(math.subtract(data, data_shift), data_shift);
-			data_returns.map(function(col){ col[0] = 0; return col;});
-			
-			//sim requirement
-			var mean_daily_returns = math.mean(data_returns, 1);
-			
-			//sim requirement
-			//var cov_matrix = nj.zeros([N,N]);
-			var cov_matrix = [...Array(N)].map(x=>Array(N).fill(0));
-			for (var i=0; i < N; i++){
-				for (var j=0; j < N; j++){
-					//cov_matrix.set( i,j, cov(data_returns[i], data_returns[j]) );
-					cov_matrix[i][j] = cov(data_returns[i], data_returns[j]);
-				}
-			}
-			
-			return [asset_prices, mean_daily_returns, cov_matrix];
-		}
-		
-		///////////////////////////////////////////////////////////////////////////////////////////
-		
-		function simulate(stocks, data, free_rate, total_amount, prune){
-			
-			var [asset_prices, mean_daily_returns, cov_matrix] = preprocess(stocks, data);
-			
-			// 250 market days in a year
-			var annual_periods = 250;
-			var eps = 20000;
-			
-			var allocate = (total_amount != 0);
-			var N = stocks.length;
-			
-			// MONTE CARLO DATA
-			var [returns, risks, sharpes, portfolios, stock_counts] = [[], [], [], [], []];
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/mathjs/6.6.0/math.min.js');
 
-			for (var i=0; i < eps; i++){
-				
-				//generate random portfolio weights summing to 1
-				//var weights = nj.random(N);
-				//weights = nj.divide(weights, weights.sum()).tolist();
-				var weights = math.random([N]);
-				weights = math.dotDivide(weights, math.sum(weights));
-				
-				var updated_weights;
-				if (allocate == true){
-					[updated_weights, stock_count] = stocks_allocation(total_amount, asset_prices, weights);
-					stock_counts.push(stock_count);
-				}else{
-					updated_weights = weights;
-				}
-				
-				//Calculate expected return and volatility of portfolio
-				var [pret, pstd] = calc_ret_std(updated_weights, mean_daily_returns, cov_matrix);
+function arr_shift(arr, n) {
+	arr.splice(-1, n);
+	arr.unshift(...Array(n).fill(0));
+	return arr;
+}
 
-				//Convert results to annual basis, calculate Sharpe Ratio, and store them
-				var total_return = math.multiply(pret, annual_periods);
-				var total_risk = math.multiply(pstd, math.sqrt(annual_periods));
-				var sharpe_ratio = (total_return - free_rate)/total_risk;
-				
-				//////////////////////////////////////////////////////////////
-				//prune!!!
-				if (allocate){
-					var non_existent = !portfolios.includes(updated_weights); // plotting is faster
-				}else{
-					var non_existent = true; //simulation is faster
-				}
-				
-				if (prune){
-					
-					if (returns.length <= 200 && risks.length <= 200){
-						var mean_return = 0;
-						var mean_risk = 1;
-					}else{
-						var mean_return = math.mean(returns);
-						var mean_risk = math.mean(risks);
-					}
-					
-					if ( non_existent && total_return > mean_return && total_risk < mean_risk){
-						
-						returns.push(total_return);
-						risks.push(total_risk);
-						sharpes.push(sharpe_ratio);
-						portfolios.push(updated_weights);
-						
-					}
-					
-				}else{
-					if (non_existent){
-						returns.push(total_return);
-						risks.push(total_risk);
-						sharpes.push(sharpe_ratio);
-						portfolios.push(updated_weights);
-					}
-				}
-				
-				// returns.push(total_return);
-				// risks.push(total_risk);
-				// sharpes.push(sharpe_ratio);
-				// portfolios.push(updated_weights);
-				//////////////////////////////////////////////////////////////
+// Column Pairwise Covariance
+function cov(a, b) {
+	var av_a = math.mean(a);
+	var av_b = math.mean(b);
+
+	var cov = 0;
+	var n = a.length;
+	for (var i = 0; i < n; i++) {
+		cov += (a[i] - av_a) * (b[i] - av_b);
+	}
+	return (cov / (n - 1));
+}
+
+function calc_ret(weights, mean_returns) {
+	var dotprod = math.dotMultiply(mean_returns, weights);
+	return math.sum(dotprod);
+}
+
+function calc_std(weights, cov_matrix) {
+	var covdotw = math.multiply(cov_matrix, weights);
+	return math.sqrt(math.multiply(math.transpose(weights), covdotw));
+}
+
+function calc_ret_std(weights, mean_returns, cov_matrix) {
+	//Calculate return and std dev (risk)
+	ret = calc_ret(weights, mean_returns);
+	std = calc_std(weights, cov_matrix);
+	return [ret, std];
+}
+
+function calc_neg_sharpe(weights, mean_returns, cov_matrix, free_rate = 0) {
+	var [p_ret, p_std] = calc_ret_std(weights, mean_returns, cov_matrix);
+	return -(p_ret - free_rate) / p_std;
+}
+
+function stocks_allocation(total_amount, asset_prices, weights) {
+	var count = math.floor(math.dotDivide(math.multiply(total_amount, weights), asset_prices));
+	var new_weights = math.dotDivide(count, math.sum(count));
+	return [new_weights, count];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+function preprocess(stocks, data) {
+	// sim requirement
+	var N = stocks.length;
+	// sim requirement
+	var asset_prices = JSON.parse(JSON.stringify(data)).map(function (col) { return col[col.length - 1] });
+
+	var data_shift = JSON.parse(JSON.stringify(data));
+	data_shift.map(col => arr_shift(col, 1)); // equivalent of pandas dataframe shift
+	var data_returns = math.dotDivide(math.subtract(data, data_shift), data_shift);
+	data_returns.map(function (col) { col[0] = 0; return col; });
+
+	// sim requirement
+	var mean_daily_returns = math.mean(data_returns, 1);
+
+	// sim requirement
+	var cov_matrix = [...Array(N)].map(x => Array(N).fill(0));
+	for (let i = 0; i < N; i++) {
+		for (let j = 0; j < N; j++) {
+			cov_matrix[i][j] = cov(data_returns[i], data_returns[j]);
+		}
+	}
+
+	return [asset_prices, mean_daily_returns, cov_matrix];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+function simulate(stocks, data, free_rate, total_amount, prune, longshort) {
+
+	var [asset_prices, mean_daily_returns, cov_matrix] = preprocess(stocks, data);
+
+	// 250 market days in a year
+	const annual_periods = 250;
+	const eps = 20000;
+
+	const allocate = (total_amount != 0);
+	const N = stocks.length;
+
+	// MONTE CARLO DATA
+	var [returns, risks, sharpes, portfolios, stock_counts] = [[], [], [], [], []];
+
+	for (let i = 0; i < eps; i++) {
+
+		// generate random portfolio weights summing to 1 (if long) or 0 (if longshort)
+		var weights = randomWeights(N, longshort);
+
+		var updated_weights;
+		if (allocate) {
+			[updated_weights, stock_count] = stocks_allocation(total_amount, asset_prices, weights);
+			stock_counts.push(stock_count);
+		} else {
+			updated_weights = weights;
+		}
+
+		// Calculate expected return and volatility of portfolio
+		var [pret, pstd] = calc_ret_std(updated_weights, mean_daily_returns, cov_matrix);
+
+		// Convert results to annual basis, calculate Sharpe Ratio, and store them
+		var total_return = math.multiply(pret, annual_periods);
+		var total_risk = math.multiply(pstd, math.sqrt(annual_periods));
+		var sharpe_ratio = (total_return - free_rate) / total_risk;
+
+		//////////////////////////////////////////////////////////////
+		// prune!!!
+		// sim is marginally faster (by pruning duplicate portfolios reducing computation) OR plotting is faster (due to lack of O(N) portfolios checking)
+		let non_existent = allocate ? !portfolios.includes(updated_weights) : true; 
+
+		if (!non_existent) continue;
+
+		if (prune) {
+			
+			let mean_return = 0;
+			let mean_risk = 1;
+			
+			if (returns.length > 200 && risks.length > 200) {
+				mean_return = math.mean(returns);
+				mean_risk = math.mean(risks);
 			}
-			
-			return [returns, risks, sharpes, portfolios, stock_counts];
+
+			// Pruning means we only include portfolios better than the mean of individual constituents
+			// ie. better than a uniform portfolio
+			if (total_return > mean_return && total_risk < mean_risk) {
+				returns.push(total_return);
+				risks.push(total_risk);
+				sharpes.push(sharpe_ratio);
+				portfolios.push(updated_weights);
+			}
+
+		} else {
+			returns.push(total_return);
+			risks.push(total_risk);
+			sharpes.push(sharpe_ratio);
+			portfolios.push(updated_weights);
 		}
-		
-		
-		self.onmessage = function(event) {
-			[stocks, data, free_rate, total_amount, prune] = event.data;
-			console.log("Starting Simulation...");
-			var t0 = performance.now();
-			var sim_data = simulate(stocks, data, free_rate, total_amount, prune);
-			var t1 = performance.now();
-			console.log("Model run took " + (t1 - t0) + " milliseconds.");
-			console.log("Simulation Complete!");
-			
-			self.postMessage(sim_data);
-		}
+
+		// returns.push(total_return);
+		// risks.push(total_risk);
+		// sharpes.push(sharpe_ratio);
+		// portfolios.push(updated_weights);
+		//////////////////////////////////////////////////////////////
+	}
+
+	return [returns, risks, sharpes, portfolios, stock_counts];
+}
+
+
+self.onmessage = function (event) {
+	[stocks, data, free_rate, total_amount, prune, longshort] = event.data;
+	console.log("Starting Simulation...");
+	var t0 = performance.now();
+	var sim_data = simulate(stocks, data, free_rate, total_amount, prune, longshort);
+	var t1 = performance.now();
+	console.log("Model run took " + (t1 - t0) + " milliseconds.");
+	console.log("Simulation Complete!");
+
+	self.postMessage(sim_data);
+}
+
+function randomWeights(N, negative = false) {
+	var weights = math.random([N]);
+	weights = math.dotDivide(weights, math.sum(weights)); // [0, 1] sum to 1
+	if (negative) {
+		weights = math.subtract(math.dotMultiply(weights, 2), 1); // [-1, 1]
+		weights = math.subtract(weights, math.mean(weights)); // sum to 0
+	}
+	return weights;
+}
+
